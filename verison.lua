739a571fe1c9cd21fb417472fc253980b6d9826b
JuDevelopment <45209516+JuDevelopment@users.noreply.github.com>
2019-03-13 19:46:15 +0300

    Update verison.lua

--- verison.lua
+++ verison.lua
@@ -1,7 +1,4 @@
 
-ec_info = {}
+module = {}
 
-ec_info.Version = "1.5"
-
-ec_info.Agenda = "Update 1.4:\n1.Добавлена защита от возможных ESP\n2.Добавлен фикс краша(Требует доп. файлы! Читать crashfix.lua)\n3.Добавлена возможность кикать людей с рекламой в нике\n4.Добавлено уведомление админов о возможном читере\n5.Исправлены баги\nВключает датапак 26-02-19\nСкачать можно в\nDiscord Famouse:https://discord.gg/rFdQwzm\nGitHub:https://github.com/Afireal/gmod_enemycleaner/blob/master/enemy_cleaner.rar\nPatch 1.4a:\nИз-за багов с кастомными чатами, закомментирована проверка на худы\nPatch 1.4b:\nМелкие исправления"
-ec_info.LogCenter = "https://discordapp.com/api/webhooks/555281119013240853/I4zQushuYRVMUcESw2LSn0EGyhbUrdvRw3stv-A9Vg2RHtNzhdOP3egrgFvRtKLSTKRi"
+module.Version = "2.1"
